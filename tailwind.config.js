3f64a9dd1b97f3b402a3c0b1db8a6cc29b9b4158
Leonardo <leodeveloper12@gmail.com>
2025-03-27 16:13:27 -0300

    Finalizando a primeira tela da Dev hamburgueria

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{html,js}"],
   theme: {
+    fontFamily: {
+      "sans": ["Roboto", "sans-serifi"]
+    },
     extend: {
       backgroundImage:{
         "home": "url('/assets/bg.png')"
